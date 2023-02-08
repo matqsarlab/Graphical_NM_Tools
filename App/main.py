1afef1040c5d58c99989085a42fadf179eca2b86
matqsarlab <m.balicki@qsarlab.com>
2023-02-08 11:05:22 +0100

    Created DFT Frame

--- App/main.py
+++ App/main.py
@@ -338,6 +338,29 @@ class App(customtkinter.CTk):
             self.consoletextbox.configure(state="disabled")
 
             self.save_button.configure(state="normal")
+        else:
+            print(self._check_var.get())
+            # _f = filedialog.askopenfilename()
+            # o = open(_f)
+            # self._xyz = o.readlines()[2:]
+            # self._xyz = "".join(self._xyz)
+            # o.close()
+
+            # nproc = str(self.spinbox_1.get())
+            # ram = str(self.spinbox_2.get())
+            # charge = str(self.spinbox_3.get())
+            # multiplicity = str(self.spinbox_4.get())
+            # basis = str(self.spinbox_5.get())
+
+            # txt = dft_read(nproc, ram, charge, multiplicity, basis, self._xyz)
+
+            # self.consoletextbox.configure(state="normal")
+            # self.consoletextbox.delete("0.0", "end")
+            # self.consoletextbox.insert("0.0", txt)
+            # self.consoletextbox.configure(state="disabled")
+
+            # self.save_button.configure(state="normal")
+            pass
 
     def xyz2gaussian_save(self):
         fileName = filedialog.asksaveasfilename()
